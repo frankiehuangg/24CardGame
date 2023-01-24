2ac453deb619d7de64cae21d22cc115127e59a11
frankiehuangg <frankie.huangg@gmail.com>
2023-01-24 20:00:49 +0700

    add: random input, change permutation function, save to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
 
+using namespace std::chrono;
 using namespace std;
 
 char input[16];
+
+int integer[4];
+int chosen[4];
+int total = 0;
+
 vector<float> numbers;
+vector<string> answers;
+
+set<vector<float>> permutations;
 
 float convert(char c)
+// Convert char input to number
 {
+	if (c == '*')
+		return (rand() % 13) + 1;
 	if (c == 'A' || c == 'a')
 		return 1;
 	if (c == 'J' || c == 'j')
@@ -15,33 +27,55 @@ float convert(char c)
 		return 12;
 	if (c == 'K' || c == 'k')
 		return 13;
-	if (c > 0x2f && c < 0x3a)
+	if (c > 0x30 && c < 0x3a)
 		return c-0x30;
-	printf("Input tidak valid, silahkan ulangi!\n");
+	printf("Invalid input, please try again!\n");
 	exit(0);
 }
 
 void readInput()
+// Main function to read input
 {	
+	srand(time(0));
+
+	printf("Please input 4 valid cards (or * to randomize one card) seperated by space.\n");
+
 	fgets(input, 16, stdin);
+
+	printf("\n");
 	
 	int i = 0;
+	int count = 0;
 	while(input[i] != 0x0)
 	{
-		if (input[i] == '1' && input[i+1] == '0')
+		if (input[i] == '1' && input[i+1] != ' ')
 		{
-			numbers.push_back(10);
+			if (input[i+1]-0x30 > 3)
+			{
+				printf("Invalid input, please try again!\n");
+				exit(0);
+			}
+			numbers.push_back(10 + input[i+1]-0x30);
+			count++;
 			i++;
 		}
 		else if (input[i] != ' ' && input[i] != 0xa)
 		{
 			numbers.push_back(convert(input[i]));
+			count++;
 		}
 		i++;
+	}
+	
+	if (count != 4)
+	{
+		printf("Invalid input, please try again!\n");
+		exit(0);
 	}
 }
 
 float operate(float a, float b, int op)
+// Operate between 2 floats
 {
 	if (op == 0)
 		return a + b;
@@ -49,67 +83,75 @@ float operate(float a, float b, int op)
 		return a - b;
 	if (op == 2)
 		return a * b;
-
-	// Handle divide by 0
-	if (b != 0)
-		return a / b;
-
-	return -3.14;
+	return a / b;
 }
 
-void printOperator(int op)
+string insertOperator(int op)
+// Change int to char
 {
 	if (op == 0)
-		printf(" + ");
+		return " + ";
 	else if (op == 1)
-		printf(" - ");
+		return " - ";
 	else if (op == 2)
-		printf(" * ");
-	else		
-		printf(" / ");
+		return " * ";
+	return " / ";
 }
 
-void printResult(int j, int k, int l, int type)
+void findResult(int j, int k, int l, int type)
+// Push result to vector
 {
-	if (type == 1 || type == 2 || type == 3) printf("(");
-	if (type == 1) printf("(");
+	stringstream answer;
 
-	printf("%d", (int)numbers[0]);
-	printOperator(j);
+	if (type == 1 || type == 2 || type == 3) answer << "(";
+	if (type == 1) answer << "(";
 
-	if (type == 2 || type == 4 || type == 5) printf("(");
-	if (type == 5) printf("(");
+	answer << integer[0];
+	answer << insertOperator(j);
 
-	printf("%d", (int)numbers[1]);
+	if (type == 2 || type == 4 || type == 5) answer << "(";
+	if (type == 5) answer << "(";
 
-	if (type == 1 || type == 3) printf(")");
+	answer << integer[1];
 
-	printOperator(k);
+	if (type == 1 || type == 3) answer << ")";
 
-	if (type == 3 || type == 4) printf("(");
+	answer << insertOperator(k);
 
-	printf("%d", (int)numbers[2]);
+	if (type == 3 || type == 4) answer << "(";
 
-	if (type == 1 || type == 2 || type == 5) printf(")");
-	if (type == 2) printf(")");
-
-	printOperator(l);
-	printf("%d", (int)numbers[3]);
-
-	if (type == 3 || type == 4 || type == 5) printf(")");
-	if (type == 4) printf(")");
-
-	printf("\n");
-}
-
-int main()
-{
-	readInput();
-
-	int total = 0;
+	answer << integer[2];
 	
-	do {
-		float result = 0;
+	if (type == 1 || type == 2 || type == 5) answer << ")";
+	if (type == 2) answer << ")";
+
+	answer << insertOperator(l);
+	answer << integer[3];
+
+	if (type == 3 || type == 4 || type == 5) answer << ")";
+	if (type == 4) answer << ")";
+
+	answer << "\n";
+
+	answers.push_back(answer.str());
+}
+
+void swap(int a, int b)
+// Swap 2 elements in vector
+{
+	float temp = numbers[a];
+	numbers[a] = numbers[b];
+	numbers[b] = temp;
+}
+
+void findNumbers(int l, int r)
+{
+	if (l == r && permutations.count(numbers) != 1)
+	{
+		permutations.insert(numbers);
+		
+		for (int i = 0; i < 4; i++)
+			integer[i] = numbers[i];
 
 		float a = numbers[0];
 		float b = numbers[1];
@@ -126,59 +168,101 @@ int main()
 					float f = operate(b, c, j);
 					float g = operate(c, d, k);
 
-					// printOperator(i); printOperator(j); printOperator(k); printf(" %f %f %f %f %f %f %f\n", a, b, c, d, e, f, g);
-
-					if (operate(operate(e, c, j), d, k) == 24)
+					if (operate(operate(e, c, j), d, k) == 24.0)
 					{
-						printResult(i, j, k, 1);
+						findResult(i, j, k, 1);
 						total++;
 					}
-					if (operate(operate(a, f, i), d, k) == 24)
+					if (operate(operate(a, f, i), d, k) == 24.0)
 					{
-						printResult(i, j, k, 2);
+						findResult(i, j, k, 2);
 						total++;
 					}
 					if (operate(e, g, j) == 24)
 					{
-						printResult(i, j, k, 3);
+						findResult(i, j, k, 3);
 						total++;
 					}
-					if (operate(a, operate(b, g, j), i) == 24)
+					if (operate(a, operate(b, g, j), i) == 24.0)
 					{
-						printResult(i, j, k, 4);
+						findResult(i, j, k, 4);
 						total++;
 					}
-					if (operate(a, operate(f, d, k), i) == 24)
+					if (operate(a, operate(f, d, k), i) == 24.0)
 					{
-						printResult(i, j, k, 5);
+						findResult(i, j, k, 5);
 						total++;
 					}
 				}
 			}
 		}
 
-		printf("\n");
-	} while (next_permutation(numbers.begin(), numbers.end()));
-
-	printf("%d solutions found.\n", total);
+	}
+	else
+	{
+		for (int i = l; i <= r; i++)
+		{
+			swap(l, i);
+			findNumbers(l+1, r);
+			swap(l, i);
+		}
+	}
 }
 
-/* ((a b) c) d
- * (a (b c)) d
- * (a b) (c d)
- * a (b (c d))
- * a ((b c) d)
- *
- *
- * (a b) e
- * (b c) f
- * (c d) g
- *
- * (e c) d -> (e,c,j),d,k
- * (a f) d -> (a,f,i),d,k
- * e f
- * a (b g) -> a,(b,g,j),i
- * a (f d) -> a,(f,d,k),i
- */
+int main()
+// Main function
+{
+	readInput();
+	
+	auto start = high_resolution_clock::now();
 
-		
+	findNumbers(0, 3);
+
+	// Output result
+	auto stop = high_resolution_clock::now();
+
+	if (total != 0)
+	{
+		printf("%d solutions found for numbers %d, %d, %d, and %d.\n\n", total, integer[0], integer[1], integer[2], integer[3]);
+
+		for (auto x : answers)
+			cout << x;
+	}
+	else
+	{
+		printf("No solutions found for numbers %d, %d, %d, and %d.\n\n", integer[0], integer[1], integer[2], integer[3]);
+	}
+
+	printf("\nElapsed time: ");
+
+	cout << duration_cast<microseconds>(stop - start).count();
+
+	printf(" microseconds.\n\n");
+
+	// Save to file
+	char ans;
+	printf("Would you like to save the output to a file? (Y/n)");
+	scanf("%c", &ans);
+
+	while (ans != 'y' && ans != 'Y' && ans != 'n' && ans != 'N' && ans != '\n')
+	{
+		printf("Please type a valid input!\n");
+		printf("Would you like to save the output to a file? (Y/n)\n");
+		scanf("%c", &ans);	
+	}
+
+	if (ans == 'Y' || ans == 'y' || ans == '\n')
+	{
+		printf("Insert file name: ");
+		string name; cin >> name;
+
+		ofstream file("test/" + name);
+
+		file << total << " solutions found for numbers " << integer[0] << ", " << integer[1] << ", " << integer[2] << ", and " << integer[3] << ".\n\n";
+
+		for (auto x : answers)
+			file << x;
+
+		file.close();
+	}
+}
